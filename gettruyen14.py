d6bb47c5e4bb1e25251ca00e0850ef8c06cace6a
Minh Phuong <51816054+mikloa04@users.noreply.github.com>
2022-06-22 23:27:30 +0700

    Add files via upload

---
+++ gettruyen14.py
@@ -0,0 +1,61 @@
+#Script để tải truyện từ doctruyen14.net
+#Script tải html từng chương truyện về, sau đó extract lấy text, ghi vào file Ketqua.txt
+#Version: 2.0
+#Bỏ qua DDOS protection
+#Dung module cfscrape, cai dat bang cau lenh: pip install cfscrape, pip install -U cfscrape
+#Dung module cloudscraper, pip install cloudscraper, pip install cloudscraper -U
+#pip install lxml
+#pip install bs4
+
+from bs4 import BeautifulSoup
+import cloudscraper
+import fileinput
+import random
+import time
+import os
+import array as arr
+
+#Duong dan den chuong truyen
+strURLStory='https://doctruyen14.net/giac-mo-co-that-truyen-loan-luan-2021/'
+StartChapter=1 #So chuong bat dau
+EndChapter=54 #So chuong ket thuc
+x=StartChapter    
+filenameTXT ="LL-giac-mo-co-that.txt"
+
+while (x<= EndChapter):
+    StrippedContent=""
+    ChapterURL=strURLStory+str(x)+'/'    
+    scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False})
+    response=scraper.get(ChapterURL)
+    filenameHTML =str(x)+".html"  
+    bCheckLink=0
+    open(filenameHTML, 'wb').write(response.content)
+    with open(filenameHTML, encoding="utf-8") as fp:
+        soup = BeautifulSoup(fp,"lxml")
+        try:        
+            #Luu tieu de  
+            StrippedContent="\n"           
+            #Lay noi dung chuong
+            div= soup.find(id="content")             
+            #Loai bo tag br
+            for elem in div.find_all("br"):
+                elem.replace_with(elem.text + "\n")
+              
+            #Luu Noi dung chuong
+            StrippedContent="\n"+StrippedContent+"\n"+div.text+"\n"
+            bCheckLink=1
+            
+        except:
+            print('Khong ton tai Chuong: '+str(x))
+            bCheckLink=0
+    if bCheckLink==1:  
+        with open(filenameTXT, 'a', encoding="utf-8") as handle:    
+            handle.write(StrippedContent)
+        os.remove(filenameHTML)     
+    print('Da tai ('+str(x)+'/'+str(EndChapter)+')' )    
+    x+=1             
+    #Tam dung mot chut    
+    SleepTime=random.randint(3, 8)
+    time.sleep(SleepTime)
+
+print('Hoan tat!')
