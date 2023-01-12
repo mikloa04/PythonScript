592ff4cd4e683bad00106c218f9924b89e6ed731
Minh Phuong <51816054+mikloa04@users.noreply.github.com>
2023-01-12 23:48:47 +0700

    Update extract_text_from_PDF.py

--- extract_text_from_PDF.py
+++ extract_text_from_PDF.py
@@ -1,10 +1,8 @@
-#Script extract lấy text từ file pdf, ghi vào file Ketqua_pdf.txt
-#Version: 1.0.1
-#pip install PyMuPDF
 
 
 import fitz
 import fileinput
+import os
 import array as arr
 
 #Duong dan den chuong truyen
@@ -20,13 +18,15 @@ while (x<= EndChapter):
     try:    
 
         #Lấy text từ file pdf và lưu lại
-        doc = fitz.open(filenameHTML)
-        for page in doc:
-            text = page.get_text()            
-            StrippedContent = StrippedContent +"\n" + text
-        doc.close()
-        with open(filenameTXT, 'a', encoding="utf-8") as handle:    
-            handle.write(StrippedContent)
+        if(os.path.exists(filenameHTML)):
+            doc = fitz.open(filenameHTML)
+            for page in doc:
+                text = page.get_text()            
+                StrippedContent = StrippedContent +"\n" + text
+            doc.close()
+            with open(filenameTXT, 'a', encoding="utf-8") as handle:    
+                handle.write(StrippedContent)
+            os.remove(filenameHTML)
     except:
         print('Khong ton tai Chuong: '+str(x))
     print('Da tai ('+str(x)+'/'+str(EndChapter)+')' )    
