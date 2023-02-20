5159e0d4db8afc2d908d0539fc9180e6508f3734
Minh Phuong <51816054+mikloa04@users.noreply.github.com>
2023-02-20 20:30:37 +0700

    Add files via upload

---
+++ get_foxaholic.py
@@ -0,0 +1,68 @@
+﻿#Script để tải truyện từ foxaholic
+#Script tải html từng chương truyện về, sau đó extract lấy text, ghi vào file Ketqua_foxaholic.txt
+#Version: 1.0.1
+#Dung module cloudscraper, pip install cloudscraper, pip install cloudscraper -U
+#pip install lxml
+#pip install bs4
+
+from bs4 import BeautifulSoup
+import cloudscraper
+import fileinput
+import random
+import time
+import os
+import array as arr
+
+#Duong dan den chuong truyen
+strURLStory='https://www.foxaholic.com/novel/isekai-shihai-no-skill-taker-zero-kara-hajimeru-dorei-harem/chapter-'
+StartChapter=167 #So chuong bat dau
+EndChapter=237 #So chuong ket thuc 
+
+
+x=StartChapter    
+filenameTXT ="Ketqua_foxaholic.txt"
+
+while (x<= EndChapter):
+    StrippedContent=""
+    ChapterURL=strURLStory+str(x)+"/"  
+    scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False})
+    response=scraper.get(ChapterURL)
+    filenameHTML = str(x)+".html"  
+    bCheckLink=0
+    open(filenameHTML, 'wb').write(response.content)
+    with open(filenameHTML, encoding="utf-8") as fp:
+        soup = BeautifulSoup(fp,"lxml")
+        try:
+            #Luu tieu de            
+            StrippedContent = ""
+            for title in soup.find_all('title'):
+                downloaded  =   title.get_text()
+                break
+            StrippedContent = "\n"+ downloaded
+           
+            #Lay noi dung chuong
+            div= soup.find('div', class_='reading-content')
+            
+            #Loai bo tag p
+            for elem in div.find_all("br"):
+                elem.replace_with(elem.text + "\n")
+              
+            #Luu Noi dung chuong
+            StrippedContent=div.text+"\n"
+            bCheckLink=1
+            
+        except:
+            print('Khong ton tai Chuong: '+str(x))
+            bCheckLink=0
+    if bCheckLink==1:  
+        with open(filenameTXT, 'a', encoding="utf-8") as handle:    
+            handle.write(StrippedContent)
+    if(os.path.exists(filenameHTML)):
+        os.remove(filenameHTML)
+    print('Da tai ('+str(x)+'/'+str(EndChapter)+')' )    
+    x+=1             
+    #Tam dung mot chut    
+    SleepTime=random.randint(10, 15)
+    time.sleep(SleepTime)
+
+print('Hoan tat!')
