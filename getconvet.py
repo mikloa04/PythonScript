672d6cd2d40393e5e7b392c7aa8eff1f5c0aee7b
Minh Phuong <51816054+mikloa04@users.noreply.github.com>
2023-02-28 00:00:15 +0700

    Update getconvet.py

--- getconvet.py
+++ getconvet.py
@@ -12,7 +12,8 @@ import array as arr
 
 
 LinkFile = "link_convet.txt"
-filenameTXT ="Ketqua_Convet.txt"
+filenameTXT ="Ketqua_convet.txt"
+filelog ="log_convet.txt"
 chapter = 0
 f = open(LinkFile, "r")
 for x in f:   
@@ -22,35 +23,41 @@ for x in f:
         scraper = cfscrape.CloudflareScraper()
         response= scraper.get(ChapterURL)
         filenameHTML =str(chapter)+".html"  
-        bCheckLink=0
+        bCheckLink=0        
         open(filenameHTML, 'wb').write(response.content)
         with open(filenameHTML, encoding="utf-8") as fp:
             soup = BeautifulSoup(fp,"lxml")
+            downloaded=''
             try:        
                 #Luu tieu de  
-                downloaded = soup.title.string
+                downloaded = soup.find('h1', {'id': 'chapter-heading'}).text                
                 StrippedContent="\n"+ downloaded+"\n"                
                 
                 #Lay noi dung chuong
-                div= soup.find('div', class_='reading-content')               
+                div= soup.find('div', class_='reading-content')             
+                          
                 for elem in div.find_all("p"):
                     elem.replace_with(elem.text + "\n\n") 
                 _text = div.text
                 #Luu Noi dung chuong
                 StrippedContent="\n"+StrippedContent+"\n"+_text+"\n"
                 bCheckLink=1
-                
             except:
-                print('Khong ton tai Chuong: '+str(x))
+
                 bCheckLink=0
         if bCheckLink==1:  
             with open(filenameTXT, 'a', encoding="utf-8") as handle:    
                 handle.write(StrippedContent)
-            os.remove(filenameHTML)     
-        print('Da tai ('+downloaded+')' )    
+        else:
+            StrippedContent = 'Error download '+ChapterURL+'\n'
+            with open(filelog, 'a', encoding="utf-8") as handle:    
+                handle.write(StrippedContent)
+        if(os.path.exists(filenameHTML)):
+            os.remove(filenameHTML)
+        print('Downloaded: ('+downloaded+')' )    
         chapter+=1             
         #Tam dung mot chut    
         SleepTime=random.randint(5, 10)
         time.sleep(SleepTime)
 f.close()
-print('Hoan tat!')
+print('Finished!')
