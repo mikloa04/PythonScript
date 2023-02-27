f1ad51b3df38dc1969ff11657d29aadae87526d1
Minh Phuong <51816054+mikloa04@users.noreply.github.com>
2023-02-28 00:08:25 +0700

    Update getMTC.py

--- getMTC.py
+++ getMTC.py
@@ -1,6 +1,6 @@
 #Script để tải truyện từ Metruyencv
 #Script tải html từng chương truyện về, sau đó extract lấy text, ghi vào file Ketqua_mtc.txt
-#Version: 5.0
+#Version: 1.6.0
 #Dung module playwright, pip install playwright, pip install playwright -U
 #playwright install
 #pip install PyMuPDF
@@ -18,20 +18,19 @@ StartChapter=502 #So chuong bat dau
 EndChapter=552 #So chuong ket thuc
 x=StartChapter    
 filenameTXT ="Ketqua_mtc.txt"
-bCheckLink = 0
+filelog ="log_mtc.txt"
 
 while (x<= EndChapter):
     StrippedContent=""
     ChapterURL=strURLStory+str(x)+'/'   
     filenameHTML =str(x)+".pdf"      
-    bCheckLink=0
     #Get html from url, save as pdf file
     try:    
         with sync_playwright() as p:
             browser = p.chromium.launch()
             page = browser.new_page()
             page.goto(ChapterURL)
-            page.wait_for_timeout(3000)
+            page.wait_for_timeout(9000)
             page.emulate_media(media="screen")
             page.pdf(path=filenameHTML)
             browser.close()
@@ -43,16 +42,18 @@ while (x<= EndChapter):
         doc.close()
         with open(filenameTXT, 'a', encoding="utf-8") as handle:    
             handle.write(StrippedContent)
-        bCheckLink=1
 
     except:
-        print('Khong ton tai Chuong: '+str(x))
-        bCheckLink=0
+        StrippedContent = 'Error download '+ChapterURL+'\n'
+        with open(filelog, 'a', encoding="utf-8") as handle:    
+            handle.write(StrippedContent)
 
-    if bCheckLink==1:
-        if(os.path.exists(filenameHTML)):
+    if(os.path.exists(filenameHTML)):
             os.remove(filenameHTML)
-    print('Da tai ('+str(x)+'/'+str(EndChapter)+')' )    
-    x+=1             
+    print('Downloaded ('+str(x)+'/'+str(EndChapter)+')' )    
+    x+=1
+    #Tam dung mot chut    
+    SleepTime=random.randint(10, 15)
+    time.sleep(SleepTime)
 
-print('Hoan tat!')
+print('Finished!')
