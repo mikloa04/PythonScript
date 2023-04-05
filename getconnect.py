e3cfc2c729c58b26b92187064cc9f11f59e93834
Minh Phuong <51816054+mikloa04@users.noreply.github.com>
2023-04-05 21:43:59 +0700

    Update getconnect.py

--- getconnect.py
+++ getconnect.py
@@ -1,6 +1,6 @@
 #Script để tải truyện từ truyenconect.com
 #Script tải html từng chương truyện về, sau đó extract lấy text, ghi vào file Ketqua_connect.txt
-#Version: 2.0
+#Version: 2.1
 #Bỏ qua DDOS protection
 #Dung module cfscrape, cai dat bang cau lenh: pip install cfscrape
 
@@ -15,6 +15,7 @@ import array as arr
 
 LinkFile = "link_connect.txt"
 filenameTXT ="Ketqua_connect.txt"
+filelog ="log_connect.txt"
 chapter = 0
 f = open(LinkFile, "r")
 for x in f:   
@@ -42,16 +43,19 @@ for x in f:
                 _text = div.text
                 #Luu Noi dung chuong
                 StrippedContent="\n"+StrippedContent+"\n"+_text+"\n"
-                bCheckLink=1
-                
-            except:
-                print('Khong ton tai Chuong: '+str(x))
+                bCheckLink=1                
+            except:     
                 bCheckLink=0
         if bCheckLink==1:  
             with open(filenameTXT, 'a', encoding="utf-8") as handle:    
                 handle.write(StrippedContent)
             os.remove(filenameHTML)     
-        print('Da tai ('+downloaded+')' )    
+            print('Downloaded ('+downloaded+')' )    
+        else:
+            StrippedContent = 'Error download '+ChapterURL+'\n'
+            with open(filelog, 'a', encoding="utf-8") as handle:    
+                handle.write(StrippedContent)
+            print('Download error ('+downloaded+')' )    
         chapter+=1             
         #Tam dung mot chut    
         SleepTime=random.randint(5, 10)
