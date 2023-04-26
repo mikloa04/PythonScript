53644c29d8c362cf38015c18d1bdceb2e94cb1e5
Minh Phuong <51816054+mikloa04@users.noreply.github.com>
2023-04-26 22:30:44 +0700

    Update getTTV.py

--- getTTV.py
+++ getTTV.py
@@ -1,6 +1,6 @@
 ﻿#Script để tải truyện từ Tangthuvien
 #Script tải html từng chương truyện về, sau đó extract lấy text, ghi vào file Ketqua.txt
-#Version: 2.0
+#Version: 2.1
 #Bỏ qua DDOS protection
 #Dung module cloudscraper, pip install cloudscraper, pip install cloudscraper -U
 #pip install lxml
@@ -23,6 +23,7 @@ filenameTXT ="Ketqua_ttv.txt"
 filelog ="log_ttv.txt"
 
 while (x<= EndChapter):
+    result_test = ""
     StrippedContent=""
     ChapterURL=strURLStory+str(x)+'/'    
     scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False})
@@ -50,13 +51,19 @@ while (x<= EndChapter):
         #Luu Noi dung chuong
         with open(filenameTXT, 'a', encoding="utf-8") as handle:    
             handle.write(StrippedContent)
+        result_test = 'Downloaded: ('
     else:
         StrippedContent = 'Error download '+ChapterURL+'\n'
+        result_test = 'Error download: ('
         with open(filelog, 'a', encoding="utf-8") as handle:    
             handle.write(StrippedContent)
+        
+        
     if(os.path.exists(filenameHTML)):
         os.remove(filenameHTML)
-    print('Downloaded: ('+str(x)+'/'+str(EndChapter)+')' )    
+        result_test = result_test + str(x)+'/'+str(EndChapter)+')'
+        print(result_test)
+    
     x+=1             
     #Tam dung mot chut    
     SleepTime=random.randint(10, 15)
