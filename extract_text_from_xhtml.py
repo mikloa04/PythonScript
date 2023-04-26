3599369a5fe0fe5571d1165a9d3a2dcea6b8c619
Minh Phuong <51816054+mikloa04@users.noreply.github.com>
2023-04-27 00:30:55 +0700

    Add files via upload

---
+++ extract_text_from_xhtml.py
@@ -0,0 +1,30 @@
+﻿#Script extract text from all file xhtml in current folder
+#Version: 1.0.1
+
+
+from bs4 import BeautifulSoup
+import fitz
+import fileinput
+import os
+import array as arr
+
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    if f.endswith('.xhtml'):
+        StrippedContent = ""
+        filename = f[0:len(f)-len('.xhtml')]
+        filenameTXT = filename+".txt"              
+        with open(f, encoding="utf-8") as fp:
+            soup = BeautifulSoup(fp,"lxml")
+            #extract body content
+            tag = soup.body
+ 
+            #get each string recursively
+            for content in tag.strings:
+                StrippedContent = StrippedContent + "\n\n" + content
+        
+        with open(filenameTXT, 'a', encoding="utf-8") as handle:    
+            handle.write(StrippedContent)
+        print('Complete file: '+ f )
+                    
+print('Finished!')
